40e2c896e2fba4defc29c3388fcb07fa7783dea7
Mark Berner <mark2b@gmail.com>
2016-12-10 13:12:32 +0200

    log fixed

--- log/log.go
+++ log/log.go
@@ -9,33 +9,41 @@ var (
 	Log = logging.MustGetLogger("")
 )
 
+func SetSilentMode() {
+	initialize(defaultModeFormatter(), logging.WARNING)
+}
+
+func SetInfoMode() {
+	initialize(defaultModeFormatter(), logging.INFO)
+}
+
 func SetVerboseMode() {
-	logging.SetLevel(logging.DEBUG, "")
+	initialize(defaultModeFormatter(), logging.DEBUG)
 }
 
 func SetDebugMode() {
-	format := logging.MustStringFormatter(
-		`%{color}%{level:.1s} %{module} %{time:15:04:05.000} %{shortfile} %{message}%{color:reset}`,
-	)
-	backend := logging.NewLogBackend(os.Stdout, "", 0)
-	backendFormatter := logging.NewBackendFormatter(backend, format)
-	logging.SetBackend(backendFormatter)
-	logging.SetLevel(logging.DEBUG, "")
+	initialize(debugModeFormatter(), logging.DEBUG)
 }
 
-func Start() {
-	logging.SetLevel(logging.INFO, "")
+func initialize(formatter logging.Formatter, level logging.Level) {
+	backend := logging.NewLogBackend(os.Stdout, "", 0)
+	backendFormatter := logging.NewBackendFormatter(backend, formatter)
+	logging.SetBackend(backendFormatter)
+	logging.SetLevel(level, "")
 }
 
 func init() {
-	format := logging.MustStringFormatter(
+	SetInfoMode()
+}
+
+func debugModeFormatter() logging.Formatter {
+	return logging.MustStringFormatter(
+		`%{color}%{level:.1s} %{module} %{time:15:04:05.000} %{shortfile} %{message}%{color:reset}`,
+	)
+}
+
+func defaultModeFormatter() logging.Formatter {
+	return logging.MustStringFormatter(
 		`%{level:.1s} %{module} %{time:15:04:05.000} %{message}`,
 	)
-	backend := logging.NewLogBackend(os.Stdout, "", 0)
-	backendFormatter := logging.NewBackendFormatter(backend, format)
-	logging.SetBackend(backendFormatter)
-	logging.SetLevel(logging.INFO, "")
-
-	SetDebugMode()
-	SetVerboseMode()
 }
