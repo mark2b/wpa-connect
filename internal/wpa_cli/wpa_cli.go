3baef1239b71ecb4435b2acfe3404be3bc8078a3
Mark Berner <mark2b@gmail.com>
2020-12-26 19:47:23 +0200

    fixed save config issue

--- internal/wpa_cli/wpa_cli.go
+++ internal/wpa_cli/wpa_cli.go
@@ -2,8 +2,7 @@ package wpa_cli
 
 import (
 	"fmt"
-
-	"github.com/ThomasRooney/gexpect"
+	"os/exec"
 )
 
 type WPACli struct {
@@ -11,8 +10,9 @@ type WPACli struct {
 }
 
 func (self *WPACli) SaveConfig() (e error) {
-	cmd := fmt.Sprintf("wpa_cli -i%s save_config", self.NetInterface)
-	if _, err := gexpect.Spawn(cmd); err == nil {
+	cmd := exec.Command("wpa_cli", fmt.Sprintf("-i%s", self.NetInterface), "save_config")
+
+	if err := cmd.Start(); err == nil {
 	} else {
 		e = err
 	}
